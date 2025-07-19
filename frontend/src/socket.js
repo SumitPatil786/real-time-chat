3414814132554703340d30a4b4e5a8510fd5fbac
Sumit <sumitpatil0103@gmail.com>
2025-07-19 12:17:18 +0530

    prepare frontend for Netlify deployment

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -1,6 +1,7 @@
 import { io } from "socket.io-client";
 
-export const BASE_URL = import.meta.env.VITE_API || "http://localhost:5000";
+export const BASE_URL = import.meta.env.VITE_API;
+
 export const socket = io(BASE_URL, {
 autoConnect: false,
 });
